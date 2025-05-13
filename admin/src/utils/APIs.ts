e58789b09469b8be5b53317eb071ba0647ba3d61
DhirajKarangale <dakarangale02@gmail.com>
2025-05-13 13:13:55 +0530

    Admin dashboard completed

--- admin/src/utils/APIs.ts
+++ admin/src/utils/APIs.ts
@@ -1,9 +1,10 @@
 export const base_url = `http://localhost:2002`;
 export const uri_login = `${base_url}/admin/login`;
 export const uri_organizationall = `${base_url}/organization/all`;
-
+export const uri_organizationupdate = `${base_url}/organization/update/status`;
+export const uri_allconsents = `${base_url}/consent/getByOrganization`;
+export const uri_consentsupdate = `${base_url}/consent/update/status`;
 
 
 export const uri_users = `${base_url}/userConsent/getByOrganization`;
-export const uri_allconsents = `${base_url}/consent/getByOrganization`;
 export const uri_addconsent = `${base_url}/consent/add`;
